fdb041002f0818d19bdf019e70f0b29cdbc8980a
sangjin_ha <sangjin_ha@tmax.co.kr>
2019-12-12 17:43:37 +0900

    add first test project files

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+    const config = {
+        entry: {
+            app: ['./src/index.js']
+        },
+        output: {
+            path: path.join(__dirname),
+            filename: 'dist/[name].bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    include: path.join(__dirname),
+                    exclude: /(node_modules)|(dist)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    }
+                }
+            ]
+        }
+    }
+    if (options.mode === 'development') {
+        const template = `${options.topjs}/index.html`;
+        config.plugins = [
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({ template, inject: false })
+        ];
+        config.devtool = 'inline-source-map';
+        config.devServer = {
+            hot: true, // HMR
+            host: '192.168.13.96', // host
+            // contentBase: './dist', // root path
+            stats: {
+                color: true
+            },
+            port: 9000,
+            headers: { 'Access-Control-Allow-Origin': '*' }
+        }
+    }
+
+    return config;
+}
